53c3ac3e0bad69186270f73c871799b7dd945326
Marie-JoseeTannous <99871592+Marie-JoseeTannous@users.noreply.github.com>
2022-03-11 19:08:37 +0200

    Add files via upload

---
+++ lab_6/EntryPoint.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class EntryPoint {
+    public static void main (String[] args) {
+	
+	BasicWeek bw = new BasicWeek();
+	AdvancedWeek aw= new AdvancedWeek();
+	Calculator cal= new Calculator();
+
+ 	System.out.println(calc);
+       
+    }
+}
